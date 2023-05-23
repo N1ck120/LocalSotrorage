cadb8d9bcd97520d4d78a787c84772c5ca8917ea
_ <85962372+N1ck120@users.noreply.github.com>
2023-05-23 00:37:58 +0000

    Implementado localstorage

--- code.js
+++ code.js
@@ -1,33 +1,17 @@
-function bd(){​​
-    let dd = [
-        {​​ "id":Date.now(),"nome":"will","senha":123 }​​, //0
-        {​​ "id":Date.now(),"nome":"bob","senha":2222 }​​, //1
-        {​​ "id":Date.now(),"nome":"ringo","senha":3333 }​​ //2
-       ]
-       let user = [
-                    {​​ "id":Date.now(),"nome": "Satoshi","senha": 3636 }​​
-                  ]
-       dd.push(user)
-      let n = JSON.stringify(dd);
-    localStorage.setItem("tds", n);
-    //let dados = localStorage.getItem("tds")
-    let dados = JSON.parse(window.localStorage.getItem('tds'))
-    console.log(dados[0].nome)
-    // Retrieve
-   // document.getElementById("demo").innerHTML = localStorage.getItem("todos");
-}​​
-
 // Funções Login
 function matriz(){
+  let dados
+  if(JSON.parse(window.localStorage.getItem('tds')) != null){
+    dados = JSON.parse(window.localStorage.getItem('tds'))
 
-    var dados = [
-        {​​ "id":Date.now(),"nome":"will","senha":123 }​​,
-        {​​ "id":Date.now(),"nome":"bob","senha":2222 }​​,
-        {​​ "id":Date.now(),"nome":"ringo","senha":3333 }​​
-    ]
-    
-    console.log(dados)
+  }else{
+    dados = [
+      { id:Date.now(),login:"will",senha:123 },
+      { id:Date.now(),login:"bob",senha:2222 },
+      { id:Date.now(),login:"ringo",senha:3333 }
+ ]
 
+  }
     return dados
 }
 
@@ -46,8 +30,8 @@ function login(email,pass){
   if (encontrado == false) {
     alert("Email ou senha inválidos")
   } else {
-    window.location.href = "index.html"
     alert("Login bem-sucedido!")
+    window.location.href = "paginalivre.html"
   }
 }
 
@@ -59,7 +43,9 @@ function cadastro(email, pass){
   var db = matriz()
   var id = db.length + 1
 
-  db.push({id:id, login:email, senha:senha})
+  db.push({id:Date.now(),login:email,senha:pass })
+  let n = JSON.stringify(db);
+  localStorage.setItem("tds", n);
   console.log(db)
 
   alert("Cadastro realizado com sucesso!")
